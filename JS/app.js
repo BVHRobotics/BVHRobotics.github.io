b726f7751a56e294e8a61f6b4bdb5148854526ca
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2020-10-21 19:40:43 -0700

    Small Fixes

--- JS/app.js
+++ JS/app.js
@@ -5,22 +5,22 @@ const navSlide = () => {
 
     //Nav Toggle
     burger.addEventListener('click', () => {
-        
+
         nav.classList.toggle('nav-active');
 
         //Animation
         navLink.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
-            } else{
+            } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .3}s`
             }
         });
         //Buger Animation
         burger.classList.toggle('toggle');
     });
-    
-    
+
+
 }
 
 navSlide();
