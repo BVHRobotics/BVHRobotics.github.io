ca6f7cb88b69c6f9214b9f164645dae85de876dd
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2020-10-21 19:08:17 -0700

    Set Mobile Nav Bar

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLink = document.querySelectorAll('.nav-links li')
+
+    //Nav Toggle
+    burger.addEventListener('click', () => {
+        
+        nav.classList.toggle('nav-active');
+
+        //Animation
+        navLink.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .3}s`
+            }
+        });
+        //Buger Animation
+        burger.classList.toggle('toggle');
+    });
+    
+    
+}
+
+navSlide();
