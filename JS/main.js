5c695a489f9a7b6f4d47cbf690da5af8c4fca17d
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2020-10-22 20:54:04 -0700

    JS Nav Bug Fix

--- JS/main.js
+++ JS/main.js
@@ -22,6 +22,23 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });
 
+    nav.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+        bdy.classList.toggle('bod-active');
+
+        //Animation
+        navLink.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + .3}s`
+            }
+        });
+        //Buger Animation
+        burger.classList.toggle('toggle');
+
+    });
 
 }
 
