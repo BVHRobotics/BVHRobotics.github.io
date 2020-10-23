625743f315089cf5a568494f8d3b121c2d821959
Adrian Rodriguez-Arana <adrian.r.arana@gmail.com>
2020-10-23 14:28:29 -0700

    Particle Changes

--- JS/background.js
+++ JS/background.js
@@ -21,19 +21,38 @@ function init() {
             Math.random() * 600 - 300
         );
         star.velocity = 0;
-        star.acceleration = 0.002;
+        star.acceleration = 0.001;
         starGeo.vertices.push(star);
     }
 
+    starGeo2 = new THREE.Geometry();
+    for (let i = 0; i < 7000; i++) {
+        star2 = new THREE.Vector3(
+            Math.random() * 600 - 300,
+            Math.random() * 600 - 300,
+            Math.random() * 600 - 300
+        );
+        star2.velocity = 0;
+        star2.acceleration = 0.002;
+        starGeo2.vertices.push(star2);
+    }
+
     let sprite = new THREE.TextureLoader().load('../i/star2.png');
-    
+    let sprite2 = new THREE.TextureLoader().load('../i/star3.png');
+
     let starMaterial = new THREE.PointsMaterial({
-        size: 0.7,
+        size: 0.5,
         map: sprite
     });
 
+    let starMaterial2 = new THREE.PointsMaterial({
+        size: 0.7,
+        map: sprite2
+    });
+
     stars = new THREE.Points(starGeo, starMaterial);
-    scene.add(stars);
+    stars2 = new THREE.Points(starGeo2, starMaterial2);
+    scene.add(stars, stars2);
 
     window.addEventListener("resize", onWindowResize, false);
 
@@ -59,7 +78,20 @@ function animate() {
     starGeo.verticesNeedUpdate = true;
     stars.rotation.y += 0.002;
 
+    starGeo2.vertices.forEach(p => {
+        p.velocity += p.acceleration
+        p.y -= p.velocity;
+
+        if (p.y < -200) {
+            p.y = 200;
+            p.velocity = 0;
+        }
+    });
+    starGeo2.verticesNeedUpdate = true;
+    stars2.rotation.y += 0.002;
+
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
+
 init();
